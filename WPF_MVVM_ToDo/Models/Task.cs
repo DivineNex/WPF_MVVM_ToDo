ef7c0f791e54117cd3aedfd1e48477ed0262929a
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 22:11:55 +0700

    Task constuctor

--- WPF_MVVM_ToDo/Models/Task.cs
+++ WPF_MVVM_ToDo/Models/Task.cs
@@ -5,10 +5,19 @@ namespace WPF_MVVM_ToDo.Models
 {
     internal class Task
     {
-        public string Header { get; private set; }
+        public string Title { get; private set; }
         public string Description { get; private set; }
         public DateTime StartDate { get; private set; }
         public DateTime EndDate { get; private set;}
         public eTaskStatus Status { get; private set; }
+
+        public Task(string title, string description, DateTime startDate, DateTime endDate, eTaskStatus status)
+        {
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+            Status = status;
+        }
     }
 }
