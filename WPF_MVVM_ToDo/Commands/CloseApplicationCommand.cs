caec3abdb05bd7dd1ca40fc34d5a09e51d365451
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 22:31:00 +0700

    CloseApplicationCommand

---
+++ WPF_MVVM_ToDo/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WPF_MVVM_ToDo.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
