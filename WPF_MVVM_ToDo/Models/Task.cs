6e4fc8449059d49f26f5c025bb9c8cfb7d8f9d35
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 19:07:50 +0700

    Task class, eTaskStatus enum

---
+++ WPF_MVVM_ToDo/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WPF_MVVM_ToDo.Other;
+
+namespace WPF_MVVM_ToDo.Models
+{
+    internal class Task
+    {
+        public string Header { get; private set; }
+        public string Description { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set;}
+        public eTaskStatus Status { get; private set; }
+    }
+}
---
+++ WPF_MVVM_ToDo/Other/eTaskStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_ToDo.Other
+{
+    internal enum eTaskStatus
+    {
+        Active,
+        Completed,
+        Overdue,
+        Canceled
+    }
+}
