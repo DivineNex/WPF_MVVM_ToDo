cc3230e1e6aed36171c151ecad4a1e7c34adb0eb
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 18:59:42 +0700

    ViewModel base class

---
+++ WPF_MVVM_ToDo/ViewModels/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_MVVM_ToDo.ViewModels
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
