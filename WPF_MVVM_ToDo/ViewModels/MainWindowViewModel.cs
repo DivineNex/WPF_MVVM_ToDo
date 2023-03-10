bd5efafc2997a5a69f759cf7385e26f883a7c8ae
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 22:42:03 +0700

    MainWindowViewModel and command CloseApplicationCommand binding

---
+++ WPF_MVVM_ToDo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM_ToDo.Commands;
+
+namespace WPF_MVVM_ToDo.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        public CloseApplicationCommand CloseApplicationCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new CloseApplicationCommand();
+        }
+    }
+}
