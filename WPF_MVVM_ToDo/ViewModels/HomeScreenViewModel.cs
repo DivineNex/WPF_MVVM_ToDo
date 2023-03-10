92de96f383678c86bab69b71301a3e2f5b48da79
Nikolay Makalov <nexterrible@mail.ru>
2023-03-11 02:24:14 +0700

    Creating commands objects in VM constructor

--- WPF_MVVM_ToDo/ViewModels/HomeScreenViewModel.cs
+++ WPF_MVVM_ToDo/ViewModels/HomeScreenViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using WPF_MVVM_ToDo.Commands;
 using WPF_MVVM_ToDo.Models;
 using WPF_MVVM_ToDo.ViewModels.Controls;
 
@@ -12,8 +13,14 @@ namespace WPF_MVVM_ToDo.ViewModels
     {
         public ObservableCollection<TaskCardViewModel> TaskCardViewModels { get; }
 
+        public ValigateSearchBarOnLostFocusCommand ValidateSearchBarOnLostFocusCommand { get; private set; }
+        public ValigateSearchBarOnFocusCommand ValidateSearchBarOnFocusCommand { get; private set; }
+
         public HomeScreenViewModel()
         {
+            ValidateSearchBarOnLostFocusCommand = new ValigateSearchBarOnLostFocusCommand();
+            ValidateSearchBarOnFocusCommand = new ValigateSearchBarOnFocusCommand();
+
             TaskCardViewModels = new ObservableCollection<TaskCardViewModel>();
 
             TaskCardViewModels.Add(new TaskCardViewModel(new Task("test1",
@@ -21,6 +28,30 @@ namespace WPF_MVVM_ToDo.ViewModels
                 new DateTime(2000, 12, 10),
                 new DateTime(2000, 12, 11),
                 Other.eTaskStatus.Active)));
+
+            TaskCardViewModels.Add(new TaskCardViewModel(new Task("test1",
+    "test description",
+    new DateTime(2000, 12, 10),
+    new DateTime(2000, 12, 11),
+    Other.eTaskStatus.Active)));
+
+            TaskCardViewModels.Add(new TaskCardViewModel(new Task("test1",
+    "test description",
+    new DateTime(2000, 12, 10),
+    new DateTime(2000, 12, 11),
+    Other.eTaskStatus.Active)));
+
+            TaskCardViewModels.Add(new TaskCardViewModel(new Task("test1",
+    "test description",
+    new DateTime(2000, 12, 10),
+    new DateTime(2000, 12, 11),
+    Other.eTaskStatus.Active)));
+
+            TaskCardViewModels.Add(new TaskCardViewModel(new Task("test1",
+    "test description",
+    new DateTime(2000, 12, 10),
+    new DateTime(2000, 12, 11),
+    Other.eTaskStatus.Active)));
         }
     }
 }
