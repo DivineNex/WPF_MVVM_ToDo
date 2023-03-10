45374ee44428a8fbd87d82ec46aec00858a54d82
Nikolay Makalov <nexterrible@mail.ru>
2023-03-10 22:42:28 +0700

    TaskCardViewModel

---
+++ WPF_MVVM_ToDo/ViewModels/Controls/TaskCardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using WPF_MVVM_ToDo.Models;
+
+namespace WPF_MVVM_ToDo.ViewModels.Controls
+{
+	class TaskCardViewModel : ViewModel
+    {
+		private Task _task;
+
+		public Task Task
+		{
+			get { return _task; }
+			private set { Set(ref _task, value, nameof(Task)); }
+		}
+
+		public TaskCardViewModel(Task task)
+		{
+			_task = task;
+		}
+	}
+}
