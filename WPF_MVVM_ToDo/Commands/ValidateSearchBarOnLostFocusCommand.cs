7ae3e26a0409d01bfd562128d43e838d2ec8e202
Nikolay Makalov <nexterrible@mail.ru>
2023-03-11 02:22:59 +0700

    Some commands for search bar

---
+++ WPF_MVVM_ToDo/Commands/ValidateSearchBarOnLostFocusCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_MVVM_ToDo.Commands
+{
+    class ValigateSearchBarOnLostFocusCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter)
+        {
+            var textbox = parameter as TextBox;
+
+            if (string.IsNullOrEmpty(textbox.Text))
+                textbox.Text = "Search";
+        }
+    }
+}
