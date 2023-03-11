4ba1f3c42842b27c90f285699345d621ff2f0889
Nikolay Makalov <nexterrible@mail.ru>
2023-03-11 13:58:57 +0700

    TaskCreationWindow rename

---
+++ WPF_MVVM_ToDo/Views/TaskCreationWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPF_MVVM_ToDo
+{
+    /// <summary>
+    /// Логика взаимодействия для ModalWindowView.xaml
+    /// </summary>
+    public partial class TaskCreationWindowView : Window
+    {
+        public TaskCreationWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
