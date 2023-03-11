35bf5e0ce86d7e6ce54276d27faf23a7fbf24de9
Nikolay Makalov <nexterrible@mail.ru>
2023-03-11 14:55:26 +0700

    CloseApplicationCommand changed to CloseCurrentWindowCommand

---
+++ WPF_MVVM_ToDo/Commands/CloseActiveWindowCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace WPF_MVVM_ToDo.Commands
+{
+    internal class CloseActiveWindowCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter)
+        {
+            Window activeWindow = Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+
+            activeWindow?.Close();
+        }
+    }
+}
---
+++ WPF_MVVM_ToDo/Commands/OpenTaskCreationWindowCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_ToDo.Commands
+{
+    class OpenTaskCreationWindowCommand : Command
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
