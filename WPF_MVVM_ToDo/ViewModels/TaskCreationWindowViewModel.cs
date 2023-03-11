bea81fb80e0e0c98ca311ca0b3c1c46810566b52
Nikolay Makalov <nexterrible@mail.ru>
2023-03-11 15:04:38 +0700

    CloseCurrentWindowCommand binding to task creation window

--- WPF_MVVM_ToDo/ViewModels/TaskCreationWindowViewModel.cs
+++ WPF_MVVM_ToDo/ViewModels/TaskCreationWindowViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace WPF_MVVM_ToDo.ViewModels
+﻿using WPF_MVVM_ToDo.Commands;
+
+namespace WPF_MVVM_ToDo.ViewModels
 {
     internal class TaskCreationWindowViewModel : ViewModel
     {
+        public CloseActiveWindowCommand CloseActiveWindowCommand { get; private set; }
 
+        public TaskCreationWindowViewModel()
+        {
+            CloseActiveWindowCommand = new CloseActiveWindowCommand();
+        }
     }
 }
